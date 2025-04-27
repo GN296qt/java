46067a3ad1ff66a8860449e47b73f77ad00fde00
Gorli Gomati Ramya <g.gomatiramyaa@gmail.com>
2025-04-27 19:02:04 +0530

    method overloading

---
+++ oops/inheritance/polymorphism/method.java
@@ -0,0 +1,29 @@
+package oops.inheritance.polymorphism;
+
+class Addition{
+
+    int add(int a, int b){
+        return  a + b;
+    }
+
+    double add(double a, double b){
+        return a + b;
+    }
+
+    int add(int a , int b , int c){
+        return a + b + c;
+    }
+}
+public class method {
+    public static void main(String[] args) {
+
+        Addition a = new Addition();
+        System.out.println("addition of numbers : " + a.add(2,3));
+        System.out.println("addition of numbers : " + a.add(3,4,5));        
+        System.out.println("addition of numbers : " + a.add(2.3,4.5));        
+    }
+}
+
+// addition of numbers : 5
+// addition of numbers : 12
+// addition of numbers : 6.8
