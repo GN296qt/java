ad4355db099c80d4e0ccdd59ab33a4726119fd3f
Gorli Gomati Ramya <g.gomatiramyaa@gmail.com>
2025-04-27 15:55:42 +0530

    file2 of protected modifier

---
+++ oops/inheritance/example.java
@@ -0,0 +1,23 @@
+package oops.inheritance;
+import oops.basics.modifiers.Exprotected;
+public class example extends Exprotected {
+    public static void main(String[] args) {
+        //sub class  object declaration and intialization
+        example  e1 = new example();
+        e1.display();
+        System.out.println(e1.message);
+
+    }
+}
+// protected  method
+// this is a protected variable
+// this is a protected variable
+
+
+
+
+
+
+
+
+// subclass in another package (protected modifier)
