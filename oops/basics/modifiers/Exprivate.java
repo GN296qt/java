fc4d6cfafea1635e12f7f0944cd1a06f36eed00d
Gorli Gomati Ramya <g.gomatiramyaa@gmail.com>
2025-04-27 15:18:03 +0530

    example of private modifier

---
+++ oops/basics/modifiers/Exprivate.java
@@ -0,0 +1,21 @@
+package oops.basics.modifiers;
+
+// public class
+public class Exprivate {
+    // private variable
+    private String message = "this is a private variable";
+    //private method
+    private void display(){
+        System.out.println("private method");
+        System.out.println(message);
+    }
+    //public  method
+    public static void main(String[] args) {
+        // object declration and intialization
+        Exprivate e1 = new Exprivate();
+        //accessing private varibale
+        System.out.println(e1.message);
+        //accessing private method
+        e1.display();
+    }
+}
