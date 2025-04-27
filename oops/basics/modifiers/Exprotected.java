564a5e5abe06ea7b0f1038c64d65d4421f1b0dc7
Gorli Gomati Ramya <g.gomatiramyaa@gmail.com>
2025-04-27 15:54:09 +0530

    file1 of protected modifier

---
+++ oops/basics/modifiers/Exprotected.java
@@ -0,0 +1,10 @@
+package oops.basics.modifiers;
+
+public class Exprotected {
+    protected String message  = "this is a protected variable";
+    protected void display(){
+        System.out.println("protected  method");
+        System.out.println(message);
+    }
+}
+
